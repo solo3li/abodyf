7910f357a548e2a7761944add9b035107dd99d5e
solo3li <hamed3alii.6@gmail.com>
2026-05-14 05:48:38 +0000

    good

--- msa3ed/server/obj/Debug/net10.0/Uis.Server.AssemblyInfo.cs
+++ msa3ed/server/obj/Debug/net10.0/Uis.Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Uis.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7735f9898313a8e5eac2c6909adcd38517a22c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+425fadf371c51f4273d71d9fd233764f943f844f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Uis.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Uis.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
